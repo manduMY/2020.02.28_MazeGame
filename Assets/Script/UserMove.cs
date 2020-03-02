803128e67faa3fdd5b63cfcebcb06c52e951e124
manduMY <poi31320@naver.com>
2020-03-02 21:23:41 +0900

    벽 부수기 버전 v.1.0
     이 버전을 만들기 전에 DWG Asset을 이용하여 벽 부수기 기능을 구현했는데
    물리적인 힘에 따라서 벽이 부서지는 세기가 달라 플레이어가 벽에 닺자마자
    벽이 바로 안부서졌다. 그래서 DWG Asset을 이용하지 않고 벽 부수기 기능을
    새롭게 구현했다. 마우스 포인터를 검은벽에 대고 왼쪽 마우스를 클릭하면
    벽이 부서진다. 이 버전은 물리적인 힘이 일정하다.

---
+++ Assets/Script/UserMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserMove : MonoBehaviour
+{
+    Rigidbody rigid;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        
+        rigid.AddForce(new Vector3(h, 0, v), ForceMode.Impulse);
+
+        
+        
+    }
+    void BreakBrick()
+    {
+        //GameObject temp = GameObject.Find("Cube");
+        //GameObject go = GameObject.Find("Player");
+        ////go.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        ////go.transform.position = transform.position
+        ////+ new Vector3(-0.5f, 0, 0);
+        //transform.position += transform.forward * 0.5f;
+        //go.AddComponent<Rigidbody>();
+        ////go.GetComponent<Rigidbody>().AddForce(transform.forward * 2f);
+        //go.AddComponent<DWGDestroyer>();
+
+        ////Destroy(go, 5f);
+    }
+}
